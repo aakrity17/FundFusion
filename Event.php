8c9fdd666595f643e151153c34c2183dce7464fd
laxman99 <parajulil385@gmail.com>
2023-04-27 21:12:55 +0545

    event page update with css

--- Event.php
+++ Event.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="Event.css" >
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,20 +10,42 @@
 
 </head>
 <body>
+    <div class="event-banner">
+        <h1> Events </h1>
+    </div>
+    <hr/>
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-4">    
+            <div class="card" >
+                <img class="card-img-top" src="https://d1vdjc70h9nzd9.cloudfront.net/media/campaign/208000/208591/image/5eeb103354e12.jpeg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Donate for Education</h5>
+                    <p class="card-text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout....</p>
+                    <a href="#" class="btn btn-danger">Donate</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">    
+            <div class="card" >
+                <img class="card-img-top" src="https://muslimhands.org.uk/_ui/images/36404f884e19.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Donate for Drinking water</h5>
+                    <p class="card-text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout....</p>
+                    <a href="#" class="btn btn-danger">Donate</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">    
+            <div class="card" >
+                <img class="card-img-top" src="https://bronxnet.org/media/filer_public_thumbnails/filer_public/c9/d1/c9d1ca88-5b7e-49e8-b0c5-3fa04bf0a7c5/prop-food-02.jpg__840x560_q85_crop_subsampling-2_upscale.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Donate for food</h5>
+                    <p class="card-text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout....</p>
+                    <a href="#" class="btn btn-danger">Donate</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
     
-    <h1>This is Events page</h1>
-    <h2>Developer--Laxman Parajuli</h2>
-    <?php
-    echo "my name is laxman <br>";
-$text1="lagrandee college";
-$text2="BCA 6Th sem  <br>";
-echo $text1." ".$text2;
-$num1=25;
-$num2=30.5;
-echo $num1+$num2;
-if($num1==25 and $num2==30.5)
-echo "<br> Hello World";
-?>
-    
-</body>
-</html>

4ff38bf5c2db926e2c885e72c0abd99612d85555
laxman99 <parajulil385@gmail.com>
2023-05-13 18:21:42 +0545

    Event Updated

--- Event.php
+++ Event.php
@@ -16,9 +16,9 @@
     <hr/>
     <div class="container-fluid">
     <div class="row">
-        <div class="col-4">    
-            <div class="card" >
-                <img class="card-img-top" src="https://d1vdjc70h9nzd9.cloudfront.net/media/campaign/208000/208591/image/5eeb103354e12.jpeg" alt="Card image cap">
+        <div class="col-4 event-card-layout">
+            <div class="card" style="width:26rem;">
+                <img class="card-img-top event-card-image" src="https://d1vdjc70h9nzd9.cloudfront.net/media/campaign/208000/208591/image/5eeb103354e12.jpeg" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Donate for Education</h5>
                     <p class="card-text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout....</p>
@@ -26,9 +26,9 @@
                 </div>
             </div>
         </div>
-        <div class="col-4">    
-            <div class="card" >
-                <img class="card-img-top" src="https://muslimhands.org.uk/_ui/images/36404f884e19.jpg" alt="Card image cap">
+        <div class="col-4 event-card-layout">    
+            <div class="card" style="width:26rem;">
+                <img class="card-img-top event-card-image" src="https://muslimhands.org.uk/_ui/images/36404f884e19.jpg" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Donate for Drinking water</h5>
                     <p class="card-text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout....</p>
@@ -36,9 +36,39 @@
                 </div>
             </div>
         </div>
-        <div class="col-4">    
-            <div class="card" >
-                <img class="card-img-top" src="https://bronxnet.org/media/filer_public_thumbnails/filer_public/c9/d1/c9d1ca88-5b7e-49e8-b0c5-3fa04bf0a7c5/prop-food-02.jpg__840x560_q85_crop_subsampling-2_upscale.jpg" alt="Card image cap">
+        <div class="col-4 event-card-layout">    
+            <div class="card" style="width:26rem;">
+                <img class="card-img-top event-card-image" src="https://bronxnet.org/media/filer_public_thumbnails/filer_public/c9/d1/c9d1ca88-5b7e-49e8-b0c5-3fa04bf0a7c5/prop-food-02.jpg__840x560_q85_crop_subsampling-2_upscale.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Donate for food</h5>
+                    <p class="card-text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout....</p>
+                    <a href="#" class="btn btn-danger">Donate</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4 event-card-layout">    
+            <div class="card" style="width:26rem;">
+                <img class="card-img-top event-card-image" src="https://bronxnet.org/media/filer_public_thumbnails/filer_public/c9/d1/c9d1ca88-5b7e-49e8-b0c5-3fa04bf0a7c5/prop-food-02.jpg__840x560_q85_crop_subsampling-2_upscale.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Donate for food</h5>
+                    <p class="card-text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout....</p>
+                    <a href="#" class="btn btn-danger">Donate</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4 event-card-layout">    
+            <div class="card" style="width:26rem;">
+                <img class="card-img-top event-card-image" src="https://bronxnet.org/media/filer_public_thumbnails/filer_public/c9/d1/c9d1ca88-5b7e-49e8-b0c5-3fa04bf0a7c5/prop-food-02.jpg__840x560_q85_crop_subsampling-2_upscale.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Donate for food</h5>
+                    <p class="card-text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout....</p>
+                    <a href="#" class="btn btn-danger">Donate</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4 event-card-layout">    
+            <div class="card" style="width:26rem;">
+                <img class="card-img-top event-card-image" src="https://bronxnet.org/media/filer_public_thumbnails/filer_public/c9/d1/c9d1ca88-5b7e-49e8-b0c5-3fa04bf0a7c5/prop-food-02.jpg__840x560_q85_crop_subsampling-2_upscale.jpg" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Donate for food</h5>
                     <p class="card-text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout....</p>

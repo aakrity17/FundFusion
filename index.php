21f516e3b2f37ddcc04b39830b2f44fb1c7da1f8
Aakrity Chapagai <aakritycpg@gmail.com>
2023-06-17 18:57:56 +0545

    modadkjad

--- index.php
+++ index.php
@@ -369,6 +369,7 @@ include "./admin/routeconfig.php"
 
     <!-- Membership card -->
 
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .card {

ca90921c1247fe9b792d9d631a14e681d9c1179a
Binaya KOirala <beeenaya98@gmail.com>
2023-06-28 12:30:25 +0545

    dashboard nav updated

---
+++ Membership/DashboardMembership.php
@@ -0,0 +1,20 @@
+<?php
+include "../admin/routeconfig.php";
+
+// Include the database connection file
+include "../database/Db_Connection.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/Dashboard.css">
+</head>
+<body>
+    <?php include "../admin/partials/navigation.php"; ?>
+    <h1 style="text-align: center;">Members</h1>
+</body>
+</html>
--- admin/partials/navigation.php
+++ admin/partials/navigation.php
@@ -1,6 +1,4 @@
-<?php
-include "routeconfig.php";
-?>
+
 <div class="navigation">
         <ul>
           <li>
@@ -9,54 +7,77 @@ include "routeconfig.php";
               <span class="title">FundFusion</span>
             </a>
           </li>
-          <li>
-            <a href="<?php echo $site_url ?>User/userAdd.php">
-            <span class="icon"></span>
-              <span class="title">Users</span>
-            </a>
-          </li>
+
           <li>
             <a href="<?php echo $site_url ?>admin/DashboardComponents/Homepage.php">
             <span class="icon"></span>
               <span class="title">Home</span>
             </a>
           </li>
-          <li>
-            <a href="">
-            <span class="icon"></span>
-              <span class="title">About</span>
-            </a>
-          </li>
-          <li>
-          <a href="<?php echo $site_url ?>admin/donation/create-donation.php">
-              <span class="icon"></span>
-              <span class="title">Donation</span>
-            </a>
-          </li>
-          <li>
-            <a href="<?php echo $site_url ?>admin/events/create-event.php">
-            <span class="icon"></span>              
-            <span class="title">Events</span>
-            </a>
-          </li>
+
           <li>
           <a href="<?php echo $site_url ?>admin/Adminadd.php">
               <span class="icon"></span>
               <span class="title">Admins</span>
             </a>
           </li>
+
+          <li>
+            <a href="<?php echo $site_url ?>User/userAdd.php">
+            <span class="icon"></span>
+              <span class="title">Users</span>
+            </a>
+          </li>
+
+          <li>
+          <a href="<?php echo $site_url ?>admin/donation/create-donation.php">
+              <span class="icon"></span>
+              <span class="title">Donation</span>
+            </a>
+          </li>
+
           <li>
             <a href="<?php echo $site_url ?>admin/projects/create-project.php">
             <span class="icon"></span>              
             <span class="title">Projects</span>
             </a>
           </li>
+
+          <li>
+            <a href="<?php echo $site_url ?>admin/events/create-event.php">
+            <span class="icon"></span>              
+            <span class="title">Events</span>
+            </a>
+          </li>
+
           <li>
           <a href="<?php echo $site_url ?>admin/DashboardComponents/Teams.php">
               <span class="icon"></span>
               <span class="title">Team</span>
             </a>
           </li>
+
+          <li>
+            <a href="<?php echo $site_url ?>Footer/DashboardFooter.php">
+            <span class="icon"></span>
+              <span class="title">Footer</span>
+            </a>
+          </li>
+          
+          <li>
+            <a href="<?php echo $site_url ?>Membership/DashboardMembership.php">
+            <span class="icon"></span>
+              <span class="title">Membership</span>
+            </a>
+          </li>
+         
+          <li>
+            <a href="<?php echo $site_url ?>Sponsor/DashboardSponsor.php">
+            <span class="icon"></span>
+              <span class="title">Sponsors</span>
+            </a>
+          </li>
+
           <li>
             <a href="../User/logout.php">
             <span class="icon"></span>

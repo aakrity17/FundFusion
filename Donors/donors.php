57740210285f62b276ac0809cb5fbf72552a0775
laxman99 <parajulil385@gmail.com>
2023-07-01 14:34:23 +0545

    donors sort data by amount and date conflict solved

--- Donors/donors.php
+++ Donors/donors.php
@@ -21,47 +21,46 @@
             </select>
         </div>
         <div id="unsortedItemList" class="row">
-                <?php
-                include '../database/Db_Connection.php';
+            <?php
+            include '../database/Db_Connection.php';
 
-                // Retrieve the user data from the database
-                $sql = "SELECT * FROM donors";
-                $result = $conn->query($sql);
+            // Retrieve the user data from the database
+            $sql = "SELECT * FROM donors";
+            $result = $conn->query($sql);
 
-        // Retrieve the user data from the database
-        $sql = "SELECT * FROM donors";
-        $result = $conn->query($sql);
+            // Retrieve the user data from the database
+            $sql = "SELECT * FROM donors";
+            $result = $conn->query($sql);
 
-        if ($result->num_rows > 0) {
-            // Output data of each row
-            while ($row = $result->fetch_assoc()) {
+            if ($result->num_rows > 0) {
+                // Output data of each row
+                while ($row = $result->fetch_assoc()) {
                 ?>
-               <div class="col-md-4">
-  <div class="card mb-3">
-    <div class="row g-0">
-      <div class="col-md-4">
-        <img src="../img/member2.jpg" class="img-fluid rounded-start" alt="Donor Image" style="padding-top:50px;">
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5 class="card-title"><?php echo $row["name"]; ?></h5>
-          <p class="card-text">Address: <?php echo $row["address"]; ?></p>
-          <p class="card-text">Amount: <?php echo $row["amount"]; ?></p>
-          <p class="card-text">Cause: <?php echo $row["cause"]; ?></p>
-          <p class="card-text"><small class="text-muted">Date: <?php echo $row["date"]; ?></small></p>
-          <a href="../personal.php?title=<?php echo $row['email']; ?>" class="btn btn-danger">Visit</a>
+               <div class="dataCardModule col-md-4" data-day="<?php echo $row["date"]; ?>" data-amount="<?php echo $row["amount"]; ?>">
+                    <div class="card mb-3">
+                        <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="../img/member2.jpg" class="img-fluid rounded-start" alt="Donor Image" style="padding-top:50px;">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                            <h5 class="card-title"><?php echo $row["name"]; ?></h5>
+                            <p class="card-text">Address: <?php echo $row["address"]; ?></p>
+                            <p class="card-text">Amount: <?php echo $row["amount"]; ?></p>
+                            <p class="card-text">Cause: <?php echo $row["cause"]; ?></p>
+                            <p class="card-text"><small class="text-muted">Date: <?php echo $row["date"]; ?></small></p>
+                            <a href="../personal.php?title=<?php echo $row['email']; ?>" class="btn btn-danger">Visit</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </div>
-      </div>
-    </div>
-  </div>
-</div>
-
                 <?php
-            }
-        } else {
-            echo "<p>No donors found</p>";
-        }
-        ?>
+                    }
+                } else {
+                    echo "<p>No donors found</p>";
+                }
+                ?>
     </div>
 </div>
 <script>
@@ -69,6 +68,7 @@
 				$('[name="sortDonorsList"]').change(function () {
 					if($('[name="sortDonorsList"]').val() == "amount"){
 						var cardData = $(this).parents().find('div.dataCardModule');
+                        console.log(cardData);
                         cardData.sort(function (a, b) {
                             return b.dataset.amount - a.dataset.amount
                         })
@@ -76,6 +76,7 @@
 					}
 					if($('[name="sortDonorsList"]').val() == "date"){
 						var cardData = $(this).parents().find('div.dataCardModule');
+                        console.log(cardData);
                         cardData.sort(function (a, b) {
                             return new Date($(a).data("day")) - new Date($(b).data("day"));
                         })
